42869a1627895230c4462f38e44286413e2e5b5a
Lesteur <101471593+Lesteur@users.noreply.github.com>
2025-12-24 00:55:52 +0100

    Ajoutez des fichiers projet.

---
+++ MyFrameworkProject/Program.cs
@@ -0,0 +1,14 @@
+﻿// using var game = new MyFrameworkProject.Game1();
+// game.Run();
+
+namespace MyFrameworkProject
+{
+    public static class Program
+    {
+        static void Main()
+        {
+            var game = new Application();
+            game.Run();
+        }
+    }
+}
