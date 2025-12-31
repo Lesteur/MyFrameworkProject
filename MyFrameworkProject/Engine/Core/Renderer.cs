8ef365532de8f727489a2332f7c5c5eef0a6bfe1
Lesteur <101471593+Lesteur@users.noreply.github.com>
2025-12-31 18:24:19 +0100

    Créé encapsulation de shader

--- MyFrameworkProject/Engine/Core/Renderer.cs
+++ MyFrameworkProject/Engine/Core/Renderer.cs
@@ -63,6 +63,11 @@ namespace MyFrameworkProject.Engine.Core
         /// </summary>
         public Camera UICamera => _uiCamera;
 
+        /// <summary>
+        /// Obtient le gestionnaire de shaders.
+        /// </summary>
+        public ShaderManager ShaderManager => _shaderManager;
+
         #endregion
 
         #region Constructors
